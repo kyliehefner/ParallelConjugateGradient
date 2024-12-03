767193bce498110825176654450ab6cd1adf078a
Kylie Hefner <kyliehefner@verizon.net>
2024-12-03 15:17:21 -0800

    Supporting functions and outline

--- conjugategradient.cpp
+++ conjugategradient.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
 using namespace std;
 
-int main() {
-    
+// Matrix-Vector Multiplication
+void MatrixVectorMultiply(const vector<vector<double>> &A, 
+                          const vector<double> &vec, 
+                          vector<double> &result);
 
+// Dot Product
+double DotProduct(const vector<double> &vec1, const vector<double> &vec2);
+
+// Vector Update
+void VectorUpdate(vector<double> &vec1, const vector<double> &vec2, 
+                  double scalar, bool add = true);
+
+// Main CG Algorithm
+void ConjugateGradientSolver(const vector<vector<double>> &A,
+                             const vector<double> &b,
+                             vector<double> &x);
+
+int main() {
 
 
 }
